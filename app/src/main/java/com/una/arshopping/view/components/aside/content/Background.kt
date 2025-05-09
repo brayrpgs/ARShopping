a83a3f8c92b47d1140ab64af25de0508e1a293d3
KendallFallas07 <kendallfallas2018@gmail.com>
2025-05-09 17:18:58 -0600

    Aside activity implemented, 100% / 100% Preferences activity implemented, 80% / 100%

--- app/src/main/java/com/una/arshopping/view/components/aside/content/Background.kt
+++ app/src/main/java/com/una/arshopping/view/components/aside/content/Background.kt
@@ -6,20 +6,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
-import com.una.arshopping.R
 import com.una.arshopping.view.components.aside.button.DarkModeButton
 
 @Composable
--- app/src/main/java/com/una/arshopping/view/components/preferences/PreferencesActivity.kt
+++ app/src/main/java/com/una/arshopping/view/components/preferences/PreferencesActivity.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.una.arshopping.styles.Styles
-import com.una.arshopping.view.components.preferences.content.Divisor
 import com.una.arshopping.view.components.preferences.content.MainBox
 
 class PreferencesActivity : ComponentActivity() {
