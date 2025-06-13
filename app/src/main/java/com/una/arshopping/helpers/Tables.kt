4abf9d89e35157f956be5204308a9206c986043f
BrayRPGs <85127778+brayrpgs@users.noreply.github.com>
2025-06-12 23:49:12 -0600

    Develop (#20)
    * -- Implemented SQLite local database for user management
    -- Added functions for inserting, retrieving, updating, and deleting users
    -- Defined database schema and queries in `Tables.kt`
    -- Created `LocalDataBaseHelper` for database creation and upgrades
    -- Introduced `UserRepository` for handling database operations
    -- Added example unit test for user insertion
    
    * -- add email input component
    -- add password validation
    
    * -- Added `showSupportingText` property to `TextInput`
    -- When `showSupportingText` is `true`, the height of the `TextInput` is adjusted to 75dp, otherwise it is 63dp
    -- Set `showSupportingText` to `showPasswordError.value` in `SingIn.kt`
    
    * -- update login view model
    - modify the user repository
    - update intent for social media
    - and modify tables of database

---
+++ app/src/main/java/com/una/arshopping/helpers/Tables.kt
@@ -0,0 +1,23 @@
+package com.una.arshopping.helpers
+
+/**
+ * static variables
+ */
+const val USERS_TABLE = "users"
+const val CREATE_USERS_TABLE =
+    "CREATE TABLE $USERS_TABLE (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT UNIQUE, username TEXT  UNIQUE, avatar_url TEXT,isActive BOOLEAN);"
+
+const val DATABASE_NAME = "project.db"
+const val DATABASE_VERSION = 1
+
+/***
+ * query's
+ */
+const val SELECT_ALL_USERS = "SELECT * FROM $USERS_TABLE"
+const val INSERT_USER =
+    "INSERT INTO $USERS_TABLE (id, email, username, avatar_url, isActive) VALUES (?, ?, ?, ?, ?)"
+const val UPDATE_USER =
+    "UPDATE $USERS_TABLE SET email = ?, username = ?, avatar_url = ?, isActive = ? WHERE id = ?"
+const val DELETE_USER = "DELETE FROM $USERS_TABLE"
+
+
