6c91466ba33b6b63f7e4b881dcef35e880adf786
BrayRPGs <85127778+brayrpgs@users.noreply.github.com>
2025-06-13 20:05:55 -0600

    Implemented animations in aside activity (#21)
    Implemented animations in aside activity

--- app/src/main/java/com/una/arshopping/view/components/main/components/MenuButton.kt
+++ app/src/main/java/com/una/arshopping/view/components/main/components/MenuButton.kt
@@ -7,10 +7,12 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun MenuButton(isMenuOpen: Boolean, onClick: () -> Unit){
+fun MenuButton(isMenuOpen: Boolean, onClick: () -> Unit) {
     IconButton(onClick = onClick) {
         Icon(
             imageVector = Icons.Filled.Menu, // Menu icon
