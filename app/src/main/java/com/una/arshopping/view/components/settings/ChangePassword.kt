6177025632f9e8b642a65af9a6c257cdefefcbf0
BrayRpgs <brayanymelquisedec@gmail.com>
2025-06-17 17:52:02 -0600

    -- add change password component -- improve settings activity

---
+++ app/src/main/java/com/una/arshopping/view/components/settings/ChangePassword.kt
@@ -0,0 +1,88 @@
+package com.una.arshopping.view.components.settings
+
+import android.util.Log
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.una.arshopping.styles.Styles
+
+@Composable
+fun ChangePassword(
+    colorLabel: Color = Color.Black
+) {
+    Text(
+        text = "Change Password",
+        fontSize = 22.sp,
+        fontFamily = Styles().fontFamily,
+        color = colorLabel,
+        modifier = Modifier
+            .offset(0.dp, 455.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 55.dp)
+            .height(50.dp)
+    )
+    /**
+     * elevate future function
+     */
+    var password by remember { mutableStateOf("") }
+    var isValidPassword by remember { mutableStateOf(false) }
+    OutlinedTextField(
+        onValueChange = {
+            password = it
+            val regex = Regex("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$")
+            isValidPassword = !regex.matches(password)
+        },
+        value = password,
+        modifier = Modifier
+            .offset(0.dp, 490.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 55.dp)
+            .height(57.dp),
+        label = {
+            Text(
+                text = "Write the new password",
+                fontSize = 15.sp,
+                fontFamily = Styles().fontFamily,
+                color = colorLabel,
+            )
+        },
+        shape = RoundedCornerShape(24.dp),
+        singleLine = true,
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = colorLabel,
+            unfocusedBorderColor = colorLabel
+        ),
+        isError = isValidPassword
+    )
+
+    if (isValidPassword) {
+        Log.d("Password", "Password is not valid")
+        Text(
+            text = "Password must be at least 8 characters long, contain at least one letter and one number",
+            color = Color.Red,
+            fontSize = 10.sp,
+            fontFamily = Styles().fontFamily,
+            minLines = 3,
+            modifier = Modifier
+                .offset(0.dp, 550.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 55.dp)
+                .height(50.dp)
+        )
+    }
+
+}
--- app/src/main/java/com/una/arshopping/view/components/settings/SettingsActivity.kt
+++ app/src/main/java/com/una/arshopping/view/components/settings/SettingsActivity.kt
@@ -4,25 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.una.arshopping.R
-import com.una.arshopping.styles.Styles
 
 class SettingsActivity : ComponentActivity() {
     /********************************
@@ -48,8 +30,15 @@ class SettingsActivity : ComponentActivity() {
                     DivisorSettings()
                     LabelAccount()
                     LoginMethod()
+                    ChangePassword()
+                    LogOutButton()
                 }
             }
         }
+    }
+
+    @Composable
+    fun LogOutButton() {
+
     }
 }
