7028e3e613bec22ba14bf8f93add21810c35ccff
BrayRpgs <brayanymelquisedec@gmail.com>
2025-06-17 18:02:13 -0600

    -- Add logout and delete account buttons to SettingsActivity -- Add vector drawables for logout and alert triangle icons

--- app/src/main/java/com/una/arshopping/view/components/settings/SettingsActivity.kt
+++ app/src/main/java/com/una/arshopping/view/components/settings/SettingsActivity.kt
@@ -4,7 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import coil.imageLoader
+import com.una.arshopping.R
 
 class SettingsActivity : ComponentActivity() {
     /********************************
@@ -32,6 +44,7 @@ class SettingsActivity : ComponentActivity() {
                     LoginMethod()
                     ChangePassword()
                     LogOutButton()
+                    DeleteAccount()
                 }
             }
         }
@@ -39,6 +52,37 @@ class SettingsActivity : ComponentActivity() {
 
     @Composable
     fun LogOutButton() {
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .offset(0.dp, 600.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 55.dp)
+                .height(50.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.log_out),
+                contentDescription = "image description",
+                contentScale = ContentScale.FillHeight,
+            )
+        }
+    }
 
+    @Composable
+    fun DeleteAccount() {
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .offset(0.dp, 675.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 55.dp)
+                .height(50.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.alert_triangle),
+                contentDescription = "image description",
+                contentScale = ContentScale.FillHeight,
+            )
+        }
     }
 }
